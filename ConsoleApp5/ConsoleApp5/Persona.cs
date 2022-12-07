a89a09969193e8de38f623e8fae18b6d866172aa
Oliver Infante <92893438+Inf015@users.noreply.github.com>
2022-12-06 21:48:34 -0400

    Avances

---
+++ ConsoleApp5/ConsoleApp5/Persona.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    public class Persona
+    {
+        string name { get; set; }
+        int age { get; set; }
+    }
+}
---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace ConsoleApp5
+{ 
+static void Main(string[] args)
+{
+    /* hardcode test
+    Console.WriteLine("Name");
+    string name= Console.ReadLine();
+
+    Console.WriteLine("Edad");
+    int edad = int.Parse(Console.ReadLine());
+    */
+    Persona persona = new Persona();
+        string mijson = JsonSerializer.Serialize(persona);
+
+}
+}
