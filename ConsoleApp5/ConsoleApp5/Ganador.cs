9f58d6d1d76d237fd8d723aee5e6c4a0da151fc3
Oliver Infante <92893438+Inf015@users.noreply.github.com>
2022-12-13 17:44:29 -0400

    Create and Read Json
    Agregar Funciones de escritura y lectura de archivo Json (Incompletos para el mandato)

---
+++ ConsoleApp5/ConsoleApp5/Ganador.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    public class Ganador
+    {
+        public string Name { get; set; }
+        public string Categoria {get; set; }
+    }
+}
--- ConsoleApp5/ConsoleApp5/Program.cs
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -1,19 +1,70 @@
-﻿using System.Text.Json;
+﻿using Newtonsoft.Json;
+using System.Text.Json;
 using System.Text.Json.Serialization;
 
 namespace ConsoleApp5
 { 
+    class program{
+
+   
+    private static string _path = @"C:\Users\olive\OneDrive\Desktop\json\Ganador.json";
 static void Main(string[] args)
 {
-    /* hardcode test
-    Console.WriteLine("Name");
-    string name= Console.ReadLine();
+            //var ganador = GetGanadors();
+            //serializarJson(ganador);
 
-    Console.WriteLine("Edad");
-    int edad = int.Parse(Console.ReadLine());
-    */
-    Persona persona = new Persona();
-        string mijson = JsonSerializer.Serialize(persona);
-
+            var ganador = GetGanadorFromFile();
+            DeserializeJsonFile(ganador);
 }
+        #region "Create"
+        public static void serializarJson(List<Ganador> ganador)
+        {
+             string ganadorJson = JsonConvert.SerializeObject(ganador.ToArray(), Formatting.Indented);
+            File.WriteAllText(_path, ganadorJson);
+        }
+ public static List<Ganador> GetGanadors()
+    {
+            List<Ganador> ganador = new List<Ganador>{
+                new Ganador
+                {
+                    Name = "Oliver",
+                    Categoria = "D"
+                } 
+            };
+                    
+
+ 
+    return ganador;
+      }
+        #endregion
+
+        #region "Leer"
+        public static string GetGanadorFromFile()
+        {
+            string GanadorJsonFile;
+            using (var read = new StreamReader(_path))
+            {
+                GanadorJsonFile = read.ReadToEnd();
+            }
+            return GanadorJsonFile;
+    }
+
+
+        public static void DeserializeJsonFile(string GanadorJsonFile)
+        {
+            Console.WriteLine(GanadorJsonFile);
+
+            var ganador = JsonConvert.DeserializeObject<List<Ganador>>(GanadorJsonFile);
+            Console.WriteLine(string.Format("Ganadores: Nombre {0} Categoria {1}", ganador[0].Name, ganador[0].Categoria));
+
+
+        }
+        #endregion
+    }
+
+
+
+
+
+
 }
